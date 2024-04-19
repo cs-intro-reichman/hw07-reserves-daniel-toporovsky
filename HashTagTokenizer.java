a12d7b1e4718224b2b5b8ebfd4fef87de17ad49c
daniel-toporovsky <daniel.toporovsky@runi.ac.il>
2024-04-19 18:13:33 +0300

    Task completed v1

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -14,16 +14,24 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			if (word.equals(dictionary[i])) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
+		// Pre-formatting to lowercase
+		hashtag = hashtag.toLowerCase();
 
 		// Base case: do nothing (return) if hashtag is an empty string.
         if (hashtag.isEmpty()) {
@@ -33,8 +41,12 @@ public class HashTagTokenizer {
         int N = hashtag.length();
 
         for (int i = 1; i <= N; i++) {
-		
+			String substring = hashtag.substring(0, i);
+			if (existInDictionary(substring, dictionary)) {
+				System.out.println(substring);
+				breakHashTag(hashtag.substring(i), dictionary);
+				return;
+			}			
         }
     }
-
 }
--- SpellChecker.java
+++ SpellChecker.java
@@ -11,11 +11,36 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 1) return "";
+		else return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		// Pre-formatting to lowercase
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+
+		// Checks if one of the strings is empty
+		if (word1.length() == 0) {
+			return word2.length();
+		}		
+		else if (word2.length() == 0) {
+			return word1.length();
+		}
+
+		else if (word1.charAt(0) == word2.charAt(0)) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+
+		else {
+			int i = levenshtein(tail(word1), word2);
+			int j = levenshtein(word1, tail(word2));
+			int k = levenshtein(tail(word1), tail(word2));
+
+			int min = Math.min(i, Math.min(j, k));
+
+			return 1 + min;
+		}
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -23,13 +48,31 @@ public class SpellChecker {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readString();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int min = word.length();
+		String resembles = " ";
+		for (int i = 0; i < dictionary.length; i++) {
+			int lengthOfLevenstein = levenshtein(word, dictionary[i]);
+			if ((min >= lengthOfLevenstein) && (resembles.length() != word.length() || min != lengthOfLevenstein)) {
+				min = lengthOfLevenstein;
+				resembles = dictionary[i];	
+			}
+		}
+
+		if (min > threshold) {
+			return "“" + word+ "” since the most similar word from the dictionary is at a distance greater than " + threshold;
+		}
+
+		else {
+			return "“" + resembles + "” since levenshtein(“" + word + "”, “" + resembles + "”) == " + min;
+		}
 	}
 
 }
